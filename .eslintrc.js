7335b98c26e63070e7228436c04cf860eff9fe93
leonardologmanager <leonardopegomartins@gmail.com>
2022-12-30 20:34:07 -0300

    ESlint, Typescript and Styled components configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,33 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['.eslintrc.js'],
   plugins: ['@typescript-eslint'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+      },
+    ],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
---
+++ src/screens/profileHeroScreen/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styles';
+
+function ProfileHeroScreen() {
+  return <Container />;
+}
+
+export default ProfileHeroScreen;
